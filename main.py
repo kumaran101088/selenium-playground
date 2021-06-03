c25aa8428f1a6b1e9ab32e5f1b7cdab73596b3cd
Kumaran <kumaran101088@gmail.com>
2021-06-03 17:23:20 +0530

    with cookie

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, make_response, request
 
 app = Flask(__name__)
 
@@ -9,6 +9,12 @@ def index():
 @app.route('/frames-check')
 def frames_page():
     return render_template('next_page.html')
+
+@app.route('/cookie-page')
+def sets_cookie():
+    page = make_response(render_template('see_cookie.html'))
+    page.set_cookie('test', 'this is a cookie')
+    return page
 
 if __name__ == '__main__':
     app.run(port=8080, host='127.0.0.1', debug=True)
